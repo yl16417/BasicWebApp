4da3cb50c2ccad95c2bfb3309338437335b016cd
李烨 <leah@B-T4BQJHD2-1740.local>
2021-02-02 22:18:56 +0800

    [yeli] add add and multiply

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,10 +8,20 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
 //        }else if (query.toLowerCase().contains("Imperial")) {
-        }else if (query.toLowerCase().contains("what is your name")) {
+        } else if (query.toLowerCase().contains("what is your name")) {
             return "warm-water";
         } else if (query.toLowerCase().contains("what is plus")) {
             return "+";
+        } else if (query.toLowerCase().contains("plus")) {
+            int first = query.indexOf("plus");
+            int first_num = Integer.parseInt(query.substring(0, first + 1));
+            int second_num = Integer.parseInt(query.substring(first));
+            return String.valueOf(first_num + second_num);
+        } else if (query.toLowerCase().contains("multiplied")) {
+            int first = query.indexOf("multiplied");
+            int first_num = Integer.parseInt(query.substring(0, first + 1));
+            int second_num = Integer.parseInt(query.substring(first));
+            return String.valueOf(first_num + second_num);
         }
         return "";
     }
